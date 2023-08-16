0f5fa5abb1b5fd2cf0bbbcb4985d6c43fb9fa32a
Anirudh Madhav Kulkarni <Anirudhkulkarni9094@gmail.com>
2023-08-17 00:18:58 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
 import './App.css';
 
 import Home from './Component/Home';
 
 function App() {
   return (
-        <Home/>
+    <HashRouter>
+    <Home />
+  </HashRouter>
         
   );
 }
